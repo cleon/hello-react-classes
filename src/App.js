2f9d9f99f13d08e5d7afea012b2e07dfe5cda14c
Chris Leon <christopher.leon@gmail.com>
2022-04-08 16:16:57 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -7,15 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* Only use one of these on the page, as only one will receive updates.
-        You should only have one instance of the LD SDK (ldClient). */}
-        
+       
         <LDJS />
-        <LDReact />
         
       </header>
     </div>
--- src/LDJS.js
+++ src/LDJS.js
@@ -7,28 +7,52 @@ class LDJS extends React.Component {
         super(props);
         this.state = { FF: null };
         if (!this.state.FF) {
-            const user = { key: 'abc@def.org' };
+            const user = { key: 'anon' };
             const client = LDClient.initialize(process.env.REACT_APP_LD_CLIENT_SIDE_ID, user);
-            this.state.FF = client;
+            client
+                .waitForInitialization()
+                .then(this.state.FF = client);
         }
     }
 
-    checkFlag = () => {
-        this.state.FF
-            .waitForInitialization()
-            .then(() => {
-                const flagKey = 'demoTheme';
-                const flag = this.state.FF.variation(flagKey);
-                alert(`LDJS.js: ${flagKey} is ${flag}`);
-            })
-            .catch((e) => console.error('error:', e));
+    track1Click = () => { this.getCityFlagForTrack('track1'); }
+    track2Click = () => { this.getCityFlagForTrack('track2'); }
+
+    getCityFlagForTrack = async (track) => {
+        try {
+            const LD = this.state.FF;
+            const user = {
+                key: 'anon',
+                custom: {
+                    requestKey: Math.floor(Math.random()*Date.now()),
+                    trackName: track
+                }
+            }
+
+            await LD.identify(user);
+
+            const flagKey = 'cities-flag';
+            const cities = LD.variation(flagKey).cities;
+            alert(`${user.custom.trackName}: ${cities}`);
+
+            //randomly select a city from returned array:
+            const city = cities[Math.floor(Math.random() * cities.length)];
+            const message = `Selection: ${city}`;
+
+            alert(message);
+
+        } catch (e) {
+            console.error('Error:', e);
+        }
     }
 
     render() {
         return (
             <section>
-                <h1>LDJS.js<br/>Uses the LaunchDarkly Javascript SDK</h1>
-                <button onClick={this.checkFlag}>Check Flag</button>
+                <h1>LDJS.js<br />Uses the LaunchDarkly Javascript SDK</h1>
+                <button onClick={this.track1Click}>Select from FF JSON (track1)</button>
+                <br />
+                <button onClick={this.track2Click}>Select from FF JSON (track2)</button>
             </section>
         );
     }
